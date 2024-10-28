2b7bb8b44f72289ba10c896c155277b428b18c93
Glenda189 <chicasglenda18@gmail.com>
2024-10-28 07:04:16 -0700

    most recent js changes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname,'views'));
 
 
 //Serve static files
+// app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(express.static('public'));
 app.use(express.static('images'));
 
@@ -28,13 +29,24 @@ app.get('/about', (req, res) => {
 });
 
 //projects 
-app.get('/project/:id', (req, res) => {
-    const projectId= req.params.id;
-    const project = data.projects.find(p => p.id == projectId);
-    if(project) {
+// app.get('/project/:id', (req, res, next) => {
+//     const projectId= req.params.id;
+//     const project = data.projects.find(p => p.id == projectId);
+//     if(project) {
+//         res.render('project', {project});
+//     } else {
+//         res.status(404).send('Project not found');
+//     }
+// });
+
+app.get('/project/:id', (req, res,next) => {
+    const projectId = req.params.id;
+    const project = data.projects[projectId];
+
+    if(project){
         res.render('project', {project});
     } else {
-        res.status(404).send('Project not found');
+        next();
     }
 });
 
